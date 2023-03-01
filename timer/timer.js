bdfa414a6fa3633efbc3d4154cad39a657fd981c
t1sev4 <tamikofficial@gmail.com>
2023-03-01 23:34:59 +0600

    add timer

---
+++ timer/timer.js
@@ -0,0 +1,16 @@
+var countDonwDate = new Date('june 5, 2023 00:00:00').getTime();
+var countDonwFunction = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDonwDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById('timer').innerHTML = `
+    ${days} Days ${hours} Hours ${minutes} Minute ${seconds} Seconds
+  `;
+  if (distance < 0) {
+    clearInterval(countDonwFunction);
+    document.getElementById('timer').innerHTML = 'time is end';
+  }
+}, 1000);
